a17558f9b0c2de4fe44c6f1d8d9e339cd57a8b5b
BogdanSuh <sbv.220613@gmail.com>
2026-02-27 20:32:58 +0700

    компонент карточки

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+Vue.component('task-card', {
+    template: `
+        <div class="card">
+            <h4>{{ card.title }}</h4>
+            <p>{{ card.description }}</p>
+            <small>{{ formatDate(card.deadline) }}</small>
+        </div>
+    `,
+    props: ['card'],
+    methods: {
+        formatDate(date) {
+            if (!date) return '';
+            return new Date(date).toLocaleDateString('ru-RU');
+        }
+    }
+});
+
 new Vue({
     el: '#app',
     data: {
